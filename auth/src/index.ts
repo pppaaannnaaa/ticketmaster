5dd24a753ab2575c29e987f266e95ac527964f40
pppaaannnaaa <pppaaannnaaa@gmail.com>
2021-06-02 11:32:52 +0530

    infra files added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,3 +2,10 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
+app.get("/api/users/currentuser", (req, res) => {
+  console.log("/api/users/currentuser");
+  res.send("Hi There!!");
+});
+app.listen(3001, () => {
+  console.log("listening on port 3001!!");
+});
